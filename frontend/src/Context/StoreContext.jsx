c55c8ead08778b676f541ad28d8241905bbebd22
clarence-coder <54849708+clarence-coder@users.noreply.github.com>
2026-01-04 20:15:31 -0600

    Update API URL in StoreContext

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider =(props)=>{
 
     
     const [cartItems,setCartItems] = useState({});  
-    const url = "https://food-delivery-backend-h5td.onrender.com"
+    const url = "https://food-delivery-backend-wpm8.onrender.com"
     const [token,setToken]=useState("")
     const [food_list,setFoodList]=useState([]);
 
